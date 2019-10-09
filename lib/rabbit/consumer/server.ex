8effca65c2bc3728b4897a72b82e91bf0eaa77fd
Nick Sweeting <nsweeting@gmail.com>
2019-10-09 16:19:34 -0400

    Remove logging of normal channel_down

--- lib/rabbit/consumer/server.ex
+++ lib/rabbit/consumer/server.ex
@@ -258,19 +258,15 @@ defmodule Rabbit.Consumer.Server do
   end
 
   defp channel_down(%{channel_open: false} = state, _reason), do: state
+  defp channel_down(state, :normal), do: remove_channel(state)
 
   defp channel_down(state, reason) do
     log_error(state, reason)
     remove_channel(state)
   end
 
-  defp calculate_delay(attempt) when attempt > 5 do
-    5_000
-  end
-
-  defp calculate_delay(attempt) do
-    1000 * attempt
-  end
+  defp calculate_delay(attempt) when attempt > 5, do: 5_000
+  defp calculate_delay(attempt), do: 1000 * attempt
 
   defp handle_message(state, payload, meta) do
     message = Rabbit.Message.new(state.name, state.module, state.channel, payload, meta)
--- lib/rabbit/producer/server.ex
+++ lib/rabbit/producer/server.ex
@@ -227,19 +227,15 @@ defmodule Rabbit.Producer.Server do
   end
 
   defp channel_down(%{channel_open: false} = state, _reason), do: state
+  defp channel_down(state, :normal), do: remove_channel(state)
 
   defp channel_down(state, reason) do
     log_error(state, reason)
     remove_channel(state)
   end
 
-  defp calculate_delay(attempt) when attempt > 5 do
-    5_000
-  end
-
-  defp calculate_delay(attempt) do
-    1000 * attempt
-  end
+  defp calculate_delay(attempt) when attempt > 5, do: 5_000
+  defp calculate_delay(attempt), do: 1000 * attempt
 
   defp publish(%{channel_open: false}, _msg), do: {:error, :not_connected}
 
